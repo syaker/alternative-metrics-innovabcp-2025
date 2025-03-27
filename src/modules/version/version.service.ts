e96583d2d8d26678491d946b85f6aec319378f90
sheyla.yaker <sheyla.yaker@enarahealth.com>
2025-03-27 11:09:27 -0500

    hotfix: remove simple-git, not supported in railway

--- src/modules/version/version.service.ts
+++ src/modules/version/version.service.ts
@@ -1,17 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { readFileSync } from 'fs';
-import simpleGit from 'simple-git';
+
+import { version } from '../../../package.json';
 
 @Injectable()
 export class VersionService {
-  private readonly packageJsonPath = 'package.json';
-  private readonly git = simpleGit();
-
   async getVersionInfo() {
-    const packageJson = JSON.parse(readFileSync(this.packageJsonPath, 'utf8'));
-    const version = packageJson.version;
-
-    const hash = await this.git.revparse(['HEAD']);
-    return { version: `${version}#${hash}` };
+    return version;
   }
 }
