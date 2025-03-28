b1dc6d06583af2efdd4fbce60be96a209cad49fa
sheyla.yaker <sheyla.yaker@enarahealth.com>
2025-03-27 21:10:49 -0500

    hotfix: inject modules where is needed

--- src/modules/financial-analysis/financial-analysis.module.ts
+++ src/modules/financial-analysis/financial-analysis.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { BedrockService } from '../../lib/aws/bedrock/bedrock.service';
+import { MercadoLibreService } from '../../lib/mercado-libre/mercado-libre.service';
+import { MetaService } from '../../lib/meta/meta.service';
+import { YapeService } from '../../lib/yape/yape.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { FinancialAnalysisController } from './financial-analysis.controller';
 import { FinancialAnalysisService } from './financial-analysis.service';
@@ -7,6 +10,12 @@ import { FinancialAnalysisService } from './financial-analysis.service';
 @Module({
   imports: [PrismaModule],
   controllers: [FinancialAnalysisController],
-  providers: [FinancialAnalysisService, BedrockService],
+  providers: [
+    FinancialAnalysisService,
+    BedrockService,
+    MercadoLibreService,
+    MetaService,
+    YapeService,
+  ],
 })
 export class FinancialAnalysisModule {}
--- src/modules/tasks/tasks.module.ts
+++ src/modules/tasks/tasks.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { BedrockService } from '../../lib/aws/bedrock/bedrock.service';
+import { MercadoLibreService } from '../../lib/mercado-libre/mercado-libre.service';
+import { MetaService } from '../../lib/meta/meta.service';
+import { YapeService } from '../../lib/yape/yape.service';
 import { FinancialAnalysisService } from '../financial-analysis/financial-analysis.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { TasksController } from './tasks.controller';
@@ -8,6 +11,13 @@ import { TasksService } from './tasks.service';
 @Module({
   imports: [PrismaModule],
   controllers: [TasksController],
-  providers: [TasksService, FinancialAnalysisService, BedrockService],
+  providers: [
+    TasksService,
+    FinancialAnalysisService,
+    BedrockService,
+    MercadoLibreService,
+    MetaService,
+    YapeService,
+  ],
 })
 export class TasksModule {}
